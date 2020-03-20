e5da252def970e631ff8ecc7e93a4cffc83f97f7
Matthew5U <61863679+Matthew5U@users.noreply.github.com>
2020-03-20 20:12:39 +0100

    Create config.php

---
+++ php/kurs5/config.php
@@ -0,0 +1,13 @@
+<?php
+
+    //zwroc tablice 
+    // => separator w tablicach asocjacyjnych
+    // taka tablice 4 elementow o tak ponazywachy szufladkach i wartosciach
+    return [
+        'host' => '127.0.0.1',
+        'user' => 'root',
+        'password' => '',
+        'database' => 'newsletter'
+];
+	
+// mozna pominac ? > gdy kod sie konczy php
