600589dc61dd8673118ca67711d8f14deae8f5ff
Matthew5U <61863679+Matthew5U@users.noreply.github.com>
2020-04-16 20:34:42 +0200

    Create get.php

---
+++ etap3/PHP-kurs1/get.php
@@ -0,0 +1,29 @@
+<?php
+
+    $title = "Nasza pierwsza strona";
+
+    $header = "Kalkulator";
+
+    // Metoda GET
+    /*
+    $x= isset($_GET['x'])? (float)$_GET['x'] : 0.0; // Zadaj pytanie czy jest ustawiona ta wartosc, jezeeli tak to przyjmij ta wartosc. Jezli nie to przyjmij druga wartosc
+    $y= isset($_GET['y'])? (float)$_GET['y'] : 0.0; // Zadaj pytanie czy jest ustawiona ta wartosc, jezeeli tak to przyjmij ta wartosc. Jezli nie to przyjmij druga wartosc
+    
+    // Czy x jakiekolwiek lub x rowne dokaldnie 0
+    if(($x ||  $x === 0.0) && ($y || $y ===0.0) && is_numeric($x) && is_numeric($y) ) // sprawdzamy czy istnieja zmienne i czy sa liczbami
+    {
+
+            $sum = $x + $y;
+        
+    }
+    */
+    
+
+
+
+
+
+    include_once './index.html'; // Dolaczamy nasza strone
+
+
+?>
