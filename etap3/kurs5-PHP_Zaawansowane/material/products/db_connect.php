b05a41b52e2216ce9a6266fb809c2e8e1dbfee20
Matthew5U <61863679+Matthew5U@users.noreply.github.com>
2020-04-19 12:05:15 +0200

    Create db_connect.php

---
+++ etap3/kurs5-PHP_Zaawansowane/material/products/db_connect.php
@@ -0,0 +1,13 @@
+<?php
+
+// Zmienne do otwarcia polaczenia z baza
+$server = "localhost";
+$user = "root";
+$pass = "";
+$db = "Products";
+
+$mysqli = new mysqli($server, $user, $pass, $db);
+
+
+
+
